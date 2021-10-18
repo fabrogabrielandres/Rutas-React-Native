fd99c31251e0265562a254f069a0041fedbe4cdf
FabroGabriel <fabrogabrielandres@gmail.com>
2021-10-18 11:31:35 -0500

    inicio

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { StyleSheet, Text, View } from 'react-native'
+
+const App = () => {
+  return (
+    <View>
+      <Text>holas</Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
